1c7fd38934dd5de0abb8018bed3b4df5bb345b09
André Grant <andre.b.grant@gmail.com>
2023-11-29 02:31:44 +0000

    Added upload/local connection

--- data_extraction.py
+++ data_extraction.py
@@ -10,8 +10,8 @@ class DataExtractor:
         # self.engine = self.connector.init_db_engine()
         pass
 
-    def read_rds_table(self, instance, table): 
-        creds = instance.read_db_creds()
+    def read_rds_table(self, instance, table, creds_yaml): 
+        creds = instance.read_db_creds(creds_yaml)
         engine = instance.init_db_engine(creds)
         with engine.connect() as conn:
             rds_table = pd.read_sql_table(table, conn)
@@ -19,11 +19,14 @@ class DataExtractor:
 
 connector = du.DatabaseConnector()
 extractor = DataExtractor()
-df = extractor.read_rds_table(connector, 'legacy_users')
+df = extractor.read_rds_table(connector, 'legacy_users', 'db_creds.yaml')
 print(df)
 cleaner = dc.DataCleaning()
 cleaner.clean_user_data(df)
 
-#for step 8, create new db_Creds for local database, use du to make new databse connector
-connector.upload_to_db(df, )
+
+connector1 = du.DatabaseConnector()
+creds1 = connector1.read_db_creds('db_creds_local.yaml')
+engine = connector1.init_db_engine(creds1)
+connector1.upload_to_db(df, 'sales_data')
 # %%
